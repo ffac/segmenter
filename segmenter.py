0855e68198bd7ab2dc2a950362fdd096d9db6926
Michael Rüttgers <mr@westwerk.ac>
2016-01-23 11:23:59 +0100

    Initial commit

---
+++ segmenter.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+
+import os
+import argparse
+import fiona
+from shapely.geometry import Point, shape, asShape
+import json
+import pprint
+
+def main(params):
+    os.makedirs(params['dest_dir'], exist_ok=True)
+
+    segments = []
+
+    unknown = {
+        "basename": "unknown",
+        "nodes": {}
+    }
+
+    #point = Point(6.1041687, 50.7746345)
+
+    for file in params['shape_file']:
+        fc = fiona.open(file)
+        for feature in fc:
+            bn = os.path.basename(file)
+            segments.append({
+              "polygon": asShape(feature['geometry']),
+              "basename" : bn,
+              "nodes": {}
+            })
+
+    for file in params['nodes_file']:
+        with open(file, 'r') as file_handle:
+            nodedb = json.load(file_handle)
+            for id,n in nodedb["nodes"].items():
+                if ("nodeinfo" in n) \
+                        and ("location" in n["nodeinfo"])\
+                        and ("longitude" in n["nodeinfo"]["location"])\
+                        and ("latitude" in n["nodeinfo"]["location"]):
+                    point = Point(n["nodeinfo"]["location"]["longitude"], n["nodeinfo"]["location"]["latitude"])
+                    contained = False
+                    for segment in segments:
+                        if (segment["polygon"].contains(point)):
+                            segment["nodes"][id] = n
+                            contained = True
+                            break
+                    if not contained:
+                        unknown["nodes"][id] = n
+                else:
+                    unknown["nodes"][id] = n
+
+    segments.append(unknown);
+
+    for segment in segments:
+        print("Name: " + segment["basename"])
+        for id, n in segment["nodes"].items():
+            hostname = ""
+            if "nodeinfo" in n and "hostname" in n["nodeinfo"]:
+                hostname = n["nodeinfo"]["hostname"]
+            print("  " + id + " ("+hostname+")")
+        print("")
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-d', '--dest-dir', action='store',
+                        help='Write output to destination directory',
+                        required=True)
+
+    parser.add_argument('-s', '--shape-file',
+                        help='Segment polygon',
+                        nargs='+', default=[], metavar='FILE')
+
+    parser.add_argument('-n', '--nodes-file',
+                        help='Nodes file',
+                        nargs='+', default=[], metavar='FILE')
+
+    options = vars(parser.parse_args())
+    main(options)
---
+++ test.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+./segmenter.py -d segments -n nodes.json -s shapefiles/ffac_segment02_wgs84.shp shapefiles/ffac_segment03_wgs84.shp shapefiles/ffac_segment04_wgs84.shp
