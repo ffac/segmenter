24ead3397fb28e6620c40866c672241fbc9efbdf
Jan Niehusmann <jan@gondor.com>
2016-04-13 11:54:07 +0200

    watchdog: ignore links to other supernodes

--- watchdog.py
+++ watchdog.py
@@ -26,33 +26,33 @@ class Watchdog:
         self.batman_parser = parser.batman.BatmanParser("/sys/kernel/debug/batman_adv")
 
     def check_segment(self, segment):
-        print("Checking segment {} on supernode {}".format(segment, self.nodename))
+        #print("Checking segment {} on supernode {}".format(segment, self.nodename))
         segment_config = self.config['segments'][segment]
         batman_if = segment_config['batman']
 
         gws = self.batman_parser.gateways(batman_if)
 
         for gw in gws:
-            if gw.gateway in segment_config['gateways']:
-                print("matching gw: {}".format(gw))
-            else:
-                print("bad gw: {}".format(gw))
-                other_seg = self.find_segment_of_gateway(gw.gateway)
-                if other_seg:
-                    print("shortcut {} <-> {} detected".format(segment, other_seg))
+            interface_config = self.config['interfaces'][gw.interface]
+            if not interface_config['ignore']:
+                if gw.gateway in segment_config['gateways']:
+                    print("matching gw: {}".format(gw))
                 else:
-                    print("link from segment {} to unknown gateway {} detected".format(segment, gw.gateway))
-                try:
-                    socketname = self.config['fastd-sockets'][gw.interface]
-                    fastd_status = self.fastd_parser.status(socketname)
-                    peer = self.fastd_parser.peer_for_mac(fastd_status, gw.nexthop)[0]
-                    if peer:
-                        print("peer is {} (IP: {}, MACs: {})".format(peer[1]['name'], peer[1]['address'], peer[1]['connection']['mac_addresses']))
-                        print('key "{}";'.format(peer[0]))
-                except Exception as e:
-                    print("error finding fastd key: {}".format(e))
-
-        print("\n")
+                    print("bad gw: {}".format(gw))
+                    other_seg = self.find_segment_of_gateway(gw.gateway)
+                    if other_seg:
+                        print("shortcut {} <-> {} detected".format(segment, other_seg))
+                    else:
+                        print("link from segment {} to unknown gateway {} detected".format(segment, gw.gateway))
+                    try:
+                        socketname = interface_config['fastd_socket']
+                        fastd_status = self.fastd_parser.status(socketname)
+                        peer = self.fastd_parser.peer_for_mac(fastd_status, gw.nexthop)[0]
+                        if peer:
+                            print("peer is {} (IP: {}, MACs: {})".format(peer[1]['name'], peer[1]['address'], peer[1]['connection']['mac_addresses']))
+                            print('key "{}";'.format(peer[0]))
+                    except Exception as e:
+                        print("error finding fastd key: {}".format(e))
 
     def find_segment_of_gateway(self, gw):
         for seg in self.config['segments']:
