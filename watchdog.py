2810bf9bb356626502e5ce21139388514afbc114
Jan Niehusmann <jan@gondor.com>
2016-04-16 21:24:01 +0200

    watchdog: add slack notifications

--- watchdog.py
+++ watchdog.py
@@ -7,6 +7,7 @@ import json
 import time
 import parser.fastd
 import parser.batman
+import requests
 
 def main(params):
     config = json.load(params['config_file'])
@@ -25,6 +26,12 @@ class Watchdog:
         self.fastd_parser = parser.fastd.FastdParser(self.nodeconfig['fastd_path'])
         self.batman_parser = parser.batman.BatmanParser("/sys/kernel/debug/batman_adv")
         self.known_shorts = {}
+        webhook_file = self.config.get("slack-webhook-url-file")
+        self.webhook_url = None
+        if webhook_file != None:
+            with open(webhook_file, 'r') as f:
+                self.webhook_url = f.read().strip()
+            print("Sending alerts to slack!")
 
     def check_segment(self, segment):
         #print("Checking segment {} on supernode {}".format(segment, self.nodename))
@@ -41,24 +48,30 @@ class Watchdog:
                 else:
                     print("bad gw: {}".format(gw))
                     other_seg = self.find_segment_of_gateway(gw.gateway)
+                    msg = ""
                     if other_seg:
-                        print("shortcut {} <-> {} detected".format(segment, other_seg))
+                        msg += ("shortcut {} <-> {} detected\n".format(segment, other_seg))
                     else:
-                        print("link from segment {} to unknown gateway {} detected".format(segment, gw.gateway))
+                        msg += ("link from segment {} to unknown gateway {} detected\n".format(segment, gw.gateway))
                     try:
                         socketname = interface_config['fastd_socket']
                         fastd_status = self.fastd_parser.status(socketname)
                         peer = self.fastd_parser.peer_for_mac(fastd_status, gw.nexthop)[0]
                         if peer:
-                            print("peer is {} (IP: {}, MACs: {})".format(peer[1]['name'], peer[1]['address'], peer[1]['connection']['mac_addresses']))
-                            print('key "{}";'.format(peer[0]))
+                            msg += ("peer is {} (IP: {}, MACs: {})\n".format(peer[1]['name'], peer[1]['address'], peer[1]['connection']['mac_addresses']))
+                            msg += ('key "{}";\n'.format(peer[0]))
                             known = self.known_shorts.get(peer[0])
-                            if known == None or known.get('segment') != segment:
+                            if self.webhook_url != None and known == None or known.get('segment') != segment:
                                 print("New entry - send alert!") # TODO implement alert
                                 self.known_shorts[peer[0]] = { 'segment': segment }
+                                payload= { 'username': 'watchdog on '+self.nodename, 'text': "```" + msg + "```", 'icon_emoji': ':dog2' }
+                                r=requests.post(self.webhook_url,data=json.dumps(payload))
+                                print("{} {}".format(r.status_code, r.text))
 
                     except Exception as e:
                         print("error finding fastd key: {}".format(e))
+
+                    print(msg)
 
     def find_segment_of_gateway(self, gw):
         for seg in self.config['segments']:
