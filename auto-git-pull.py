49180efaec3b32254c69c34963f362b4ba8346d5
Jan Niehusmann <jan@gondor.com>
2016-05-09 11:40:32 +0200

    simple webhook server to trigger git reloads

---
+++ auto-git-pull.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+
+import http.server
+import socketserver
+import subprocess
+import os
+import sys
+
+PORT = 11684
+
+GITBASEDIR="/etc/fastd/.peers/fastd-peers-clients"
+
+os.chdir(GITBASEDIR)
+
+if not os.path.isdir("."):
+    sys.exit("bad repo basedir")
+
+if len(subprocess.check_output(["git", "status", "--porcelain"])) != 0:
+    sys.exit("git is not clean")
+
+class WebhookHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        response ="ok\n".encode("utf-8")
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain")
+        self.send_header("Content-Length", str(len(response)))
+        self.end_headers()
+        self.wfile.write(response)
+        # TODO: rate-limit
+        self.pull_from_github()
+
+    def do_POST(self):
+        # handle POST just like GET
+        self.do_GET()
+
+    def reload(self):
+        for seg in range(2,7):
+            fn = "/var/run/fastd.{0:02}-clients.pid".format(seg)
+            subprocess.call(["pkill", "-HUP", "-F", fn])
+        for fn in ["/var/run/fastd.01-clients-v4.pid","/var/run/fastd.01-clients-v6.pid"]:
+            subprocess.call(["pkill", "-USR2", "-F", fn])
+
+    def pull_from_github(self):
+        old_commit = subprocess.check_output(["git", "rev-parse", "HEAD"])
+        subprocess.call(["git", "pull"])
+        new_commit = subprocess.check_output(["git", "rev-parse", "HEAD"])
+        if old_commit != new_commit:
+            self.reload()
+
+Handler = WebhookHTTPRequestHandler
+
+httpd = socketserver.TCPServer(("", PORT), Handler)
+
+print("serving at port", PORT)
+httpd.serve_forever()
+
