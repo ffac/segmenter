707c20bf4e1b397288a1725ef1c0e35039f37ff4
Jan Niehusmann <jan@gondor.com>
2016-11-17 22:37:00 +0100

    load data via https

--- run_segmenter.sh
+++ run_segmenter.sh
@@ -1,8 +1,8 @@
 DATE=$(date +"%Y%m%d-%H%M")
 for sn in `seq -w 01 06`; do ssh root@$sn.nodes.freifunk-aachen.de 'for a in /tmp/fastd-*client*.sock; do nc -U "$a"; done'; done | jq -s 'map(.peers)|map(with_entries(select(.value.connection.mac_addresses|length > 0)|{key : .value.connection.mac_addresses[],value : .key}))|add' >data/keys-$DATE.json
 
-curl -L --compressed http://data.aachen.freifunk.net/nodes.json >data/nodes-$DATE.json
-curl -L --compressed http://data.aachen.freifunk.net/graph.json >data/graph-$DATE.json
+curl -L --compressed https://data.aachen.freifunk.net/nodes.json >data/nodes-$DATE.json
+curl -L --compressed https://data.aachen.freifunk.net/graph.json >data/graph-$DATE.json
 
 ./segmenter.py -d segments -a aliases/supernodes.json -n data/nodes-$DATE.json -g data/graph-$DATE.json -s shapefiles.json  -k data/keys-$DATE.json -f ../fastd-peers-clients/
 
