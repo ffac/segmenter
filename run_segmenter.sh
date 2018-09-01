26198c24471bfbd61f510bdb36df9be9efd65f16
Jan Niehusmann <jan@gondor.com>
2018-09-01 10:49:54 +0200

    fix for empty fastd sockets

--- run_segmenter.sh
+++ run_segmenter.sh
@@ -1,5 +1,5 @@
 DATE=$(date +"%Y%m%d-%H%M")
-for sn in `seq -w 01 06`; do ssh root@$sn.nodes.freifunk-aachen.de 'for a in /tmp/fastd-*client*.sock; do nc -U "$a"; done'; done | jq -s 'map(.peers)|map(with_entries(select(.value.connection.mac_addresses|length > 0)|{key : .value.connection.mac_addresses[],value : .key}))|add' >data/keys-$DATE.json
+for sn in `seq -w 01 06`; do ssh root@$sn.nodes.freifunk-aachen.de 'for a in /tmp/fastd-*client*.sock; do nc -U "$a"; echo; done'; done | jq -s 'map(.peers)|map(with_entries(select(.value.connection.mac_addresses|length > 0)|{key : .value.connection.mac_addresses[],value : .key}))|add' >data/keys-$DATE.json
 
 curl -L --compressed https://data.aachen.freifunk.net/nodes.json >data/nodes-$DATE.json
 curl -L --compressed https://data.aachen.freifunk.net/graph.json >data/graph-$DATE.json
